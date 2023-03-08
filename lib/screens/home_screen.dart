ef250a095e40584d8b00731312013a92e93afce4
Azizul Hakim <aziz.swe71@gmail.com>
2023-03-08 16:17:08 +0600

    some changes in free exam button

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,20 +3,21 @@ import 'package:project/data/category_data.dart';
 import 'package:project/units/const.dart';
 import '../widgets/navigation_bar.dart';
 import '../widgets/notice_ticker.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    AppDetails _appDetails = AppDetails();
+    AppDetails appDetails = AppDetails();
     return Scaffold(
       // App Bar Start //
       appBar: AppBar(
-        backgroundColor: _appDetails.mainColor,
+        backgroundColor: appDetails.mainColor,
         leading: const Icon(Icons.menu),
         actions: [
-          Image.asset(_appDetails.accImage,
+          Image.asset(appDetails.accImage,
             width: 40,
           ),
           const SizedBox(
@@ -36,7 +37,7 @@ class HomeScreen extends StatelessWidget {
             height: 180,
             width: double.infinity,
             decoration: BoxDecoration(
-                color: _appDetails.mainColor,
+                color: appDetails.mainColor,
                 borderRadius: const BorderRadius.only(
                   bottomLeft: Radius.circular(100),
                   bottomRight: Radius.circular(100),
@@ -45,7 +46,7 @@ class HomeScreen extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Image.asset(_appDetails.headerImage,
+                Image.asset(appDetails.headerImage,
                   width: 80,
                 ),
                 const SizedBox(
@@ -57,10 +58,10 @@ class HomeScreen extends StatelessWidget {
                   child: Center(
                     child: Padding(
                       padding: const EdgeInsets.all(8.0),
-                      child: Text(_appDetails.headerText,
+                      child: Text(appDetails.headerText,
                         textAlign: TextAlign.justify,
                         style: TextStyle(
-                          color: _appDetails.white,
+                          color: appDetails.white,
                           fontSize: 20,
                           fontWeight: FontWeight.bold,
                         ),
@@ -76,10 +77,10 @@ class HomeScreen extends StatelessWidget {
             height: 20,
           ),
           // Notice Ticker Start //
-          // noticeTicker(_appDetails.noticeText,
-          //   _appDetails.noticeTicker,
-          // _appDetails.noticeImage
-          // ),
+          noticeTicker(appDetails.noticeText,
+            appDetails.noticeTicker,
+          appDetails.noticeImage
+          ),
           // Notice Ticker End //
           const SizedBox(
             height: 20,
@@ -103,7 +104,7 @@ class HomeScreen extends StatelessWidget {
                       color: categoryList[index]['color'],
                       boxShadow: [
                         BoxShadow(
-                            color: _appDetails.black.withOpacity(0.1),
+                            color: appDetails.black.withOpacity(0.1),
                             offset: Offset.zero,
                             blurRadius: 10,
                             spreadRadius: 0.1)
@@ -122,7 +123,7 @@ class HomeScreen extends StatelessWidget {
                           Text(
                             categoryList[index]['label'],
                             style: TextStyle(
-                              color: _appDetails.white,
+                              color: appDetails.white,
                               fontSize: 18,
                               fontWeight: FontWeight.bold,
                             ),
@@ -137,23 +138,23 @@ class HomeScreen extends StatelessWidget {
           ),
           // Category Section End //
           const SizedBox(
-            height: 40,
+            height: 20,
           ),
           // Free Exam Button
           ElevatedButton(
             style: ElevatedButton.styleFrom(
               elevation: 10,
-              shadowColor: _appDetails.mainColor,
+              shadowColor: appDetails.mainColor,
               padding: const EdgeInsets.only(
                   left: 30, right: 30, top: 10, bottom: 10),
-              backgroundColor: _appDetails.mainColor,
+              backgroundColor: appDetails.mainColor,
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(10),
               ),
             ),
             onPressed: () {},
-            child:  Text(_appDetails.examButtonText,
-              style: TextStyle(color: _appDetails.white, fontSize: 25),
+            child:  Text(appDetails.examButtonText,
+              style: TextStyle(color: appDetails.white, fontSize: 25),
             ),
           ),
         ],
--- lib/units/const.dart
+++ lib/units/const.dart
@@ -3,7 +3,7 @@ class AppDetails {
   // String Section //
   String headerText = 'বিসিএস, ব্যাংকসহ সকল সরকারি চাকরি পরীক্ষার প্রস্তুতি ও লাইভ মডেল টেস্ট এখন এক অ্যাপএ !';
   String noticeText = 'আমাদের অ্যাপ থেকে কোন কোর্স কিনতে সমস্যা হলে কল করুন এই নাম্বারেঃ ০১৭৫৮-১৯৫৩২৪ !!!';
-  String noticeTicker = '   Notice';
+  String noticeTicker = 'Notice';
   String examButtonText = 'Free Exam';
 
   // Image Section //
